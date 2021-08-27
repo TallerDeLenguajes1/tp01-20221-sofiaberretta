a1fbc99bd399bcb1aa89e5bc02ccc03ec0ecadf6
Ana Sofia Berretta Gali <anasofiaberrettagali@gmail.com>
2021-08-27 14:43:53 -0300

    Problema 2 resuelto

--- TrabajoPractico1/TrabajoPractico1/Controllers/HomeController.cs
+++ TrabajoPractico1/TrabajoPractico1/Controllers/HomeController.cs
@@ -28,6 +28,10 @@ namespace TrabajoPractico1.Controllers
             return View();
         }
 
+        public IActionResult Problema2()
+        {
+            return View();
+        }
 
         public string Problema1(string num)
         {
@@ -48,6 +52,36 @@ namespace TrabajoPractico1.Controllers
                 return "#ERROR El numero ingresado es demasiado grande.";
             }
             catch (Exception ex)
+            {
+                return $"#ERROR {ex.Message}";
+            }
+
+        }
+
+        public string CalculoProblema2(string num1, string num2)
+        {
+            try
+            {
+                int division;
+                int dividendo = int.Parse(num1);
+                int divisor = int.Parse(num2);
+
+                division = dividendo / divisor;
+                return $"La division entre {dividendo} / {divisor} es {division}";
+            }
+            catch (FormatException)
+            {
+                return "#ERROR No se ha ingresado un numero.";
+            }
+            catch (OverflowException)
+            {
+                return "#ERROR El numero ingresado es demasiado grande.";
+            }
+            catch (DivideByZeroException)
+            {
+                return $"#ERROR No se puede dividir por cero.";
+            }
+            catch(Exception ex)
             {
                 return $"#ERROR {ex.Message}";
             }
